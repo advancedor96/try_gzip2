47c0dd8a067640bc3c228c8237141793705dc665
丁丁 <dingding@bxb.tw>
2018-12-20 11:56:46 +0800

    沒有gzip

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,19 @@
-const CompressionWebpackPlugin = require('compression-webpack-plugin')
-const productionGzipExtensions = ['js', 'css']
+// const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// const productionGzipExtensions = ['js', 'css']
 
 module.exports = {
   baseUrl: './',
-  productionSourceMap: false,
-  configureWebpack: {
-    plugins: [
-      new CompressionWebpackPlugin({
-        filename: '[path].gz[query]', // 第2版的compression-webpack-plugin 要把 asset 改成 filename
-        algorithm: 'gzip',
-        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
-        threshold: 10240,
-        minRatio: 0.8
-      })
-    ]
-
-  }
+  productionSourceMap: false
+  // configureWebpack: {
+  //   plugins: [
+  //     new CompressionWebpackPlugin({
+  //       filename: '[path].gz[query]', // 第2版的compression-webpack-plugin 要把 asset 改成 filename
+  //       algorithm: 'gzip',
+  //       test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+  //       threshold: 10240,
+  //       minRatio: 0.8
+  //     })
+  //   ]
+  // }
 
 }
