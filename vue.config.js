037a1ba7a783f41c27659776694dbfc468d013ee
丁丁 <dingding@bxb.tw>
2018-12-20 11:35:22 +0800

    first

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+
+module.exports = {
+  baseUrl: './',
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new CompressionWebpackPlugin({
+        filename: '[path].gz[query]', // 第2版的compression-webpack-plugin 要把 asset 改成 filename
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8
+      })
+    ]
+
+  }
+
+}
